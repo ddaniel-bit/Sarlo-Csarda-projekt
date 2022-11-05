cf5ce78f9afecc487d0ecd38fb9dbad3ce6aae8c
ddaniel-bit <67413475+ddaniel-bit@users.noreply.github.com>
2022-11-06 00:44:26 +0100

    végleges verzió

---
+++ script.js
@@ -0,0 +1,251 @@
+function l1() {
+    myDivObj = document.getElementById("l1")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("l1").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("l2").style.backgroundColor = "transparent";
+        document.getElementById("l3").style.backgroundColor = "transparent";
+        document.getElementById("l4").style.backgroundColor = "transparent";
+        document.getElementById("l5").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("l1").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "transparent";
+    }
+}
+function l2() {
+    myDivObj = document.getElementById("l2")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("l1").style.backgroundColor = "transparent";
+        document.getElementById("l2").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("l3").style.backgroundColor = "transparent";
+        document.getElementById("l4").style.backgroundColor = "transparent";
+        document.getElementById("l5").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("l2").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "transparent";
+    }
+}
+function l3() {
+    myDivObj = document.getElementById("l3")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("l1").style.backgroundColor = "transparent";
+        document.getElementById("l2").style.backgroundColor = "transparent";
+        document.getElementById("l3").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("l4").style.backgroundColor = "transparent";
+        document.getElementById("l5").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("l3").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "transparent";
+    }
+}
+function l4() {
+    myDivObj = document.getElementById("l4")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("l1").style.backgroundColor = "transparent";
+        document.getElementById("l2").style.backgroundColor = "transparent";
+        document.getElementById("l3").style.backgroundColor = "transparent";
+        document.getElementById("l4").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("l5").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("l4").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "transparent";
+    }
+}
+function l5() {
+    myDivObj = document.getElementById("l5")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("l1").style.backgroundColor = "transparent";
+        document.getElementById("l2").style.backgroundColor = "transparent";
+        document.getElementById("l3").style.backgroundColor = "transparent";
+        document.getElementById("l4").style.backgroundColor = "transparent";
+        document.getElementById("l5").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("levesetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("l5").style.backgroundColor = "transparent";
+        document.getElementById("levesetel").style.backgroundColor = "transparent";
+    }
+}
+function f1() {
+    myDivObj = document.getElementById("f1")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("f1").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("f2").style.backgroundColor = "transparent";
+        document.getElementById("f3").style.backgroundColor = "transparent";
+        document.getElementById("f4").style.backgroundColor = "transparent";
+        document.getElementById("f5").style.backgroundColor = "transparent";
+        document.getElementById("f6").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("f1").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "transparent";
+    }
+}
+function f2() {
+    myDivObj = document.getElementById("f2")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("f1").style.backgroundColor = "transparent";
+        document.getElementById("f2").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("f3").style.backgroundColor = "transparent";
+        document.getElementById("f4").style.backgroundColor = "transparent";
+        document.getElementById("f5").style.backgroundColor = "transparent";
+        document.getElementById("f6").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("f2").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "transparent";
+    }
+}
+function f3() {
+    myDivObj = document.getElementById("f3")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("f1").style.backgroundColor = "transparent";
+        document.getElementById("f2").style.backgroundColor = "transparent";
+        document.getElementById("f3").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("f4").style.backgroundColor = "transparent";
+        document.getElementById("f5").style.backgroundColor = "transparent";
+        document.getElementById("f6").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("f3").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "transparent";
+    }
+}
+function f4() {
+    myDivObj = document.getElementById("f4")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("f1").style.backgroundColor = "transparent";
+        document.getElementById("f2").style.backgroundColor = "transparent";
+        document.getElementById("f3").style.backgroundColor = "transparent";
+        document.getElementById("f4").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("f5").style.backgroundColor = "transparent";
+        document.getElementById("f6").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("f4").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "transparent";
+    }
+}
+function f5() {
+    myDivObj = document.getElementById("f5")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("f1").style.backgroundColor = "transparent";
+        document.getElementById("f2").style.backgroundColor = "transparent";
+        document.getElementById("f3").style.backgroundColor = "transparent";
+        document.getElementById("f4").style.backgroundColor = "transparent";
+        document.getElementById("f5").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("f6").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("f5").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "transparent";
+    }
+}
+function f6() {
+    myDivObj = document.getElementById("f5")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("f1").style.backgroundColor = "transparent";
+        document.getElementById("f2").style.backgroundColor = "transparent";
+        document.getElementById("f3").style.backgroundColor = "transparent";
+        document.getElementById("f4").style.backgroundColor = "transparent";
+        document.getElementById("f5").style.backgroundColor = "transparent";
+        document.getElementById("f6").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("foeteletel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("f5").style.backgroundColor = "transparent";
+        document.getElementById("foeteletel").style.backgroundColor = "transparent";
+    }
+}
+function d1() {
+    myDivObj = document.getElementById("d1")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("d1").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("d2").style.backgroundColor = "transparent";
+        document.getElementById("d3").style.backgroundColor = "transparent";
+        document.getElementById("d4").style.backgroundColor = "transparent";
+        document.getElementById("d5").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("d1").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "transparent";
+    }
+}
+function d2() {
+    myDivObj = document.getElementById("d2")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("d1").style.backgroundColor = "transparent";
+        document.getElementById("d2").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("d3").style.backgroundColor = "transparent";
+        document.getElementById("d4").style.backgroundColor = "transparent";
+        document.getElementById("d5").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("d2").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "transparent";
+    }
+}
+function d3() {
+    myDivObj = document.getElementById("d3")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("d1").style.backgroundColor = "transparent";
+        document.getElementById("d2").style.backgroundColor = "transparent";
+        document.getElementById("d3").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("d4").style.backgroundColor = "transparent";
+        document.getElementById("d5").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("d3").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "transparent";
+    }
+}
+function d4() {
+    myDivObj = document.getElementById("d4")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("d1").style.backgroundColor = "transparent";
+        document.getElementById("d2").style.backgroundColor = "transparent";
+        document.getElementById("d3").style.backgroundColor = "transparent";
+        document.getElementById("d4").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("d5").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("d4").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "transparent";
+    }
+}
+function d5() {
+    myDivObj = document.getElementById("d5")
+    if (window.getComputedStyle(myDivObj).backgroundColor == "rgba(0, 0, 0, 0)") {
+        document.getElementById("d1").style.backgroundColor = "transparent";
+        document.getElementById("d2").style.backgroundColor = "transparent";
+        document.getElementById("d3").style.backgroundColor = "transparent";
+        document.getElementById("d4").style.backgroundColor = "transparent";
+        document.getElementById("d5").style.backgroundColor = "rgba(0, 130, 0, 0.4)";
+        document.getElementById("desszertetel").style.backgroundColor = "rgba(193, 193, 193, 0.2)";
+    } else {
+        document.getElementById("d5").style.backgroundColor = "transparent";
+        document.getElementById("desszertetel").style.backgroundColor = "transparent";
+    }
+}
+function torles() {
+    document.getElementById("l1").style.backgroundColor = "transparent";
+    document.getElementById("l2").style.backgroundColor = "transparent";
+    document.getElementById("l3").style.backgroundColor = "transparent";
+    document.getElementById("l4").style.backgroundColor = "transparent";
+    document.getElementById("l5").style.backgroundColor = "transparent";
+    document.getElementById("levesetel").style.backgroundColor = "transparent";
+    document.getElementById("f1").style.backgroundColor = "transparent";
+    document.getElementById("f2").style.backgroundColor = "transparent";
+    document.getElementById("f3").style.backgroundColor = "transparent";
+    document.getElementById("f4").style.backgroundColor = "transparent";
+    document.getElementById("f5").style.backgroundColor = "transparent";
+    document.getElementById("f6").style.backgroundColor = "transparent";
+    document.getElementById("foeteletel").style.backgroundColor = "transparent";
+    document.getElementById("d1").style.backgroundColor = "transparent";
+    document.getElementById("d2").style.backgroundColor = "transparent";
+    document.getElementById("d3").style.backgroundColor = "transparent";
+    document.getElementById("d4").style.backgroundColor = "transparent";
+    document.getElementById("d5").style.backgroundColor = "transparent";
+    document.getElementById("desszertetel").style.backgroundColor = "transparent";
+}
